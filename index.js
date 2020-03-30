55352da90a4c2443e4d2910a32b50997e6dd4e3b
e3mobLucasFujii <lucas@e3mob.com.br>
2020-03-30 01:42:10 -0300

    add const para alterar a porta que a aplicacao sobe

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const server = new ApolloServer({
   }), 
 });
 
-server.listen().then(({ url }) => {
+const PORT = 4000;
+server.listen({ port: PORT }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
